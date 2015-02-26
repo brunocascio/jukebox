f85e61ef97a6b5e0b8594f5fe11a32380e7f6f87
Bruno Cascio <brunocascio@gmail.com>
2015-02-25 21:53:40 -0300

    new features

--- js/main.js
+++ js/main.js
@@ -1,22 +1,28 @@
-var app = {
+var jukebox = {
   init: function(cb) {
     
     // initialize client with app credentials
     SC.initialize({
       client_id: 'da2c9944b293a284f483e3d598da60b5',
-      redirect_uri: 'http://localhost:8000/holiday-element/callback.html',
+      redirect_uri: 'http://localhost:8000/callback.html',
     });
 
     cb();
   },
-  soundCloud: {
+  app: {
     connect: function(cb) {
       SC.connect(cb);
     },
     search: function(value, cb) {
-      SC.get('/tracks', { q: value, limit: 10 }, function(tracks) {
+      SC.get('/tracks', {
+        q: value, 
+        type:'original'
+     }, function(tracks) {
         cb(tracks)
       });
+    },
+    stream: function(trackId, options, cb) {
+      SC.stream('/tracks/'+trackId, options, cb);
     }
   }
 };
